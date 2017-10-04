fbdfbb1d41f7ee59672ed12ba68705655d73eccd
Joshua Stupplebeen <jpstup@gmail.com>
2017-10-04 14:16:43 -0600

    need to work on multiple package versions specified on one line

--- src/yarnlock.js
+++ src/yarnlock.js
@@ -4,9 +4,24 @@ export default class YarnLock {
   }
 
   packages() {
-    const parsedPackages = this.rawText.split('\n\n');
-    return parsedPackages.map(p => {
+    return this.rawText.split('\n\n').map(p => {
       return p.split('\n')[0]
+    }).map(p => {
+        let name, version
+        name = p.split('@')[0]
+        version = p.split('@')[1]
+        if (version) {
+            version = version.substring(0, version.length-1) || 'no version'
+        } else {
+            version = 'no version'
+        }
+        return { name: name, version: version }
     })
+  }
+
+  packageDependencies() {
+  }
+
+  packageUris() {
   }
 }
