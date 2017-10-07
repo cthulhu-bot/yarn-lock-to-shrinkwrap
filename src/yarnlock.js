76c604e560fe4c91396cd6189f7461b5d25fcf49
Joshua Stupplebeen <jpstup@gmail.com>
2017-10-06 21:58:39 -0600

    clean up

--- src/yarnlock.js
+++ src/yarnlock.js
@@ -4,32 +4,39 @@ export default class YarnLock {
   }
 
   packages() {
-    return this.rawText.split('\n\n').map(p => {
-      return p.split('\n')[0]
-    }).map(p => {
-        let name, version
-        let parsedPackageInfo = p.split('@')
+    return this.rawText
+      .split('\n\n')
+      .map(p => {
+        return p.split('\n');
+      })
+      .map(pkg => {
+        const packageHeader = pkg[0];
+        let name, version;
+        let parsedPackageInfo = packageHeader.split('@');
         if (parsedPackageInfo.length > 2) {
-            parsedPackageInfo = parsedPackageInfo
-                .map(entry => entry.split(', '))
-                .reduce((acc, entry) => acc.concat([entry[0]]), [])
+          parsedPackageInfo = parsedPackageInfo
+            .map(entry => entry.split(', '))
+            .reduce((acc, entry) => acc.concat([entry[0]]), []);
         }
-        parsedPackageInfo[parsedPackageInfo.length - 1] = parsedPackageInfo[parsedPackageInfo.length - 1].substring(0, parsedPackageInfo[parsedPackageInfo.length - 1].length - 1)
-                        console.log(parsedPackageInfo)
-                        name = parsedPackageInfo[0]
-                        version = parsedPackageInfo[1]
-                        if (version) {
-                            version = version.substring(0, version.length-1) || 'no version'
-                        } else {
-                            version = 'no version'
-                        }
-                        return { name: name, version: version }
-                        })
-                }
-
-                packageDependencies() {
+        parsedPackageInfo[parsedPackageInfo.length - 1] = parsedPackageInfo[
+          parsedPackageInfo.length - 1
+        ].substring(
+          0,
+          parsedPackageInfo[parsedPackageInfo.length - 1].length - 1,
+        );
+        console.log(parsedPackageInfo);
+        name = parsedPackageInfo[0];
+        version = parsedPackageInfo[1];
+        if (version) {
+          version = version.substring(0, version.length - 1) || 'no version';
+        } else {
+          version = 'no version';
+        }
+        return {name: name, version: version};
+      });
   }
 
-  packageUris() {
-  }
+  packageDependencies() {}
+
+  packageUris() {}
 }
